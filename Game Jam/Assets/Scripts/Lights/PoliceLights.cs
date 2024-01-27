b7a70be7b10236f35fee59252d69aae512897eaf
Davide Fedeli <davidefedeli8899@gmail.com>
2024-01-27 18:30:44 +0100

    aggiunto End Game

--- Game Jam/Assets/Scripts/Lights/PoliceLights.cs
+++ Game Jam/Assets/Scripts/Lights/PoliceLights.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PoliceLights : MonoBehaviour
 {
@@ -26,9 +27,17 @@ public class PoliceLights : MonoBehaviour
         }
     }
 
+    public IEnumerator endGame(){
+        Debug.Log("Ending");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("UI");
+    }
     // Update is called once per frame
     void Update()
     {
+        if(parent.position.Equals(new Vector3(-7 , 16 , -13))){
+            StartCoroutine(endGame());
+        }
         transform.Rotate(0 , 2, 0 , Space.World);
         parent.position = Vector3.MoveTowards(parent.position , new Vector3(-7 , 16, -13) , 2f * Time.fixedDeltaTime);
     }
