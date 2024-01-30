f9a75230adf0437a133d0f5be106ee1f4ac5d51a
Davide Fedeli <davidefedeli8899@gmail.com>
2024-01-30 12:34:10 +0100

    build + versione finale

--- Game Jam/Assets/Scripts/musicLoop/scriptMusic.cs
+++ Game Jam/Assets/Scripts/musicLoop/scriptMusic.cs
@@ -7,20 +7,25 @@ public class scriptMusic : MonoBehaviour
 {
 
     public chillBehaviour chill;
+    public bool check = false;
     public AudioSource[] audios;
     // Start is called before the first frame update
     void Start()
     {
         audios = GetComponents<AudioSource>();
         chill = GetComponent<chillBehaviour>();        
+        audios[0].Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(chill.chill){
-            audios[0].Stop();
+        Debug.Log(audios.Length);
+        if(Input.GetButtonDown("Fire1") && !check){
+            audios[0].Stop();            
             audios[1].Play();
+            check = true;
         }
+
     }
 }
